419eebf6b9855fad91ec0835c3b46f16c498de50
Ruslan <ruslan.kurmanov@comarch.pl>
2018-11-23 16:14:59 +0100

    811 task

---
+++ src/easy/Leet811.java
@@ -0,0 +1,18 @@
+package easy;
+
+import java.util.List;
+
+public class Leet811 {
+
+    public static void main(String[] args) {
+
+        String[] example = {"9001 discuss.leetcode.com"};
+        String[] example2 = {"900 google.mail.com", "50 yahoo.com", "1 intel.mail.com", "5 wiki.org"};
+
+    }
+
+    public static List<String> subdomainVisits(String[] cpdomains) {
+
+    }
+
+}
