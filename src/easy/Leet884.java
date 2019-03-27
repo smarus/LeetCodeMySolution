463eceb12d2a5b1d3338a894f603f1f25714b7d5
Ruslan <ruslan.kurmanov@comarch.pl>
2019-03-27 16:58:05 +0100

    last changes

---
+++ src/easy/Leet884.java
@@ -0,0 +1,32 @@
+package easy;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
+public class Leet884 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        int[] simplyNumber = {2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101, 103, 107, 109, 113};
+        int n = Integer.parseInt(reader.readLine());
+        ArrayList<Integer> list = new ArrayList<>();
+        int i = 0;
+        while (n > 1) {
+            if (n % simplyNumber[i] == 0) {
+                list.add(simplyNumber[i]);
+                n = n / simplyNumber[i];
+            } else {
+                i++;
+            }
+        }
+        for (int b : list) {
+            System.out.print(b + " ");
+        }
+    }
+
+    public String[] uncommonFromSentences(String A, String B) {
+        return null;
+    }
+}
--- src/easy/Leet897.java
+++ src/easy/Leet897.java
@@ -1,8 +1,44 @@
 package easy;
 
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ * int val;
+ * TreeNode left;
+ * TreeNode right;
+ * TreeNode(int x) { val = x; }
+ * }
+ */
 
 public class Leet897 {
+
+    TreeNode ans = null;
+    TreeNode currentNode = null;
+
+    public static void main(String[] args) {
+
+    }
+
     public TreeNode increasingBST(TreeNode root) {
+        if (root == null) return null;
+        dfs(root);
+        return ans;
+    }
+
+    public void dfs(TreeNode root) {
+        if (root == null) return;
+        dfs(root.left);
+        if (ans == null) {
+            ans = new TreeNode(root.val);
+            currentNode = ans;
+        } else {
+            currentNode.right = new TreeNode(root.val);
+            currentNode = currentNode.right;
+        }
+        dfs(root.right);
 
     }
 }
