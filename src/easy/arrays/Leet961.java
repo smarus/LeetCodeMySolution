4df9bb75f93c29807b535d33a568b491bb3f89ef
Ruslan <ruslan.kurmanov@comarch.pl>
2019-06-24 15:15:15 +0200

    Divided task by theme and try to solve

---
+++ src/easy/arrays/Leet961.java
@@ -0,0 +1,29 @@
+package easy.arrays;
+
+import java.util.Arrays;
+
+//In a array A of size 2N, there are N+1 unique elements, and exactly one of these elements is repeated N times.
+//Return the element repeated N times.
+//Input: [1,2,3,3]
+//        Output: 3
+public class Leet961 {
+    public static void main(String[] args) {
+        int[] A = new int[]{1, 2, 3, 3};
+        int[] B = new int[]{2, 1, 2, 5, 3, 2};
+        int[] C = new int[]{5, 1, 5, 2, 5, 3, 5, 4};
+        System.out.println(repeatedNTimes(A));
+        System.out.println(repeatedNTimes(B));
+        System.out.println(repeatedNTimes(C));
+    }
+
+
+    public static int repeatedNTimes(int[] A) {
+        Arrays.sort(A);
+        for (int i = 0; i < A.length - 1; i++) {
+            if (A[i] == A[i + 1]) {
+                return A[i];
+            }
+        }
+        return 0;
+    }
+}
---
+++ src/easy/arrays/Leet977.java
@@ -0,0 +1,28 @@
+package easy.arrays;
+
+import java.util.Arrays;
+
+//Given an array of integers A sorted in non-decreasing order, return an array of the squares of each number, also in sorted non-decreasing order.
+public class Leet977 {
+
+    public static void main(String[] args) {
+        int[] A = new int[]{-4, -1, 0, 3, 10};
+        int[] B = new int[]{-7, -3, 2, 3, 11};
+        int[] C = sortedSquares(A);
+        int[] D = sortedSquares(B);
+        for (int i = 0; i < C.length; i++) {
+            System.out.print(D[i] + " ");
+        }
+
+    }
+
+    public static int[] sortedSquares(int[] A) {
+
+        int[] B = new int[A.length];
+        for (int i = 0; i < A.length; i++) {
+            B[i] = A[i] * A[i];
+        }
+        Arrays.sort(B);
+        return B;
+    }
+}
