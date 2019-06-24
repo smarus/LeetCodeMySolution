99b74b4e88326188aedda6348e5843d052e2d42d
Ruslan <ruslan.kurmanov@comarch.pl>
2019-06-24 16:03:11 +0200

    last code

--- src/easy/arrays/Leet985.java
+++ src/easy/arrays/Leet985.java
@@ -12,11 +12,29 @@ package easy.arrays;
 
 public class Leet985 {
     public static void main(String[] args) {
+        int[] A = new int[]{1, 2, 3, 4};
+        int[][] queries = new int[][]{{1, 0}, {-3, 1}, {-4, 0}, {2, 3}};
+
 
     }
 
     public int[] sumEvenAfterQueries(int[] A, int[][] queries) {
+        int S = 0;
+        for (int x : A)
+            if (x % 2 == 0)
+                S += x;
 
+        int[] ans = new int[queries.length];
+
+        for (int i = 0; i < queries.length; ++i) {
+            int val = queries[i][0], index = queries[i][1];
+            if (A[index] % 2 == 0) S -= A[index];
+            A[index] += val;
+            if (A[index] % 2 == 0) S += A[index];
+            ans[i] = S;
+        }
+
+        return ans;
     }
 
 }
