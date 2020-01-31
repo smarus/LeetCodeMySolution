fd9090d37c67b07588ce173b6ecb82cfde2dc98b
Ruslan Kurmanov <ruslan.kurmanov@idemia.com>
2020-01-31 17:34:55 +0100

    [31.01.2020] today was not bad

---
+++ src/easy/binara_search/Leet258.java
@@ -0,0 +1,24 @@
+package easy.binara_search;
+
+public class Leet258 {
+    public static void main(String[] args) {
+        System.out.println(addDigits(38));
+        System.out.println(addDigits(99));
+    }
+
+    public static int addDigits(int num) {
+        while (num > 9) {
+            num = sumOfDigit(num);
+        }
+        return num;
+    }
+
+    static int sumOfDigit(int num) {
+        int sum = 0;
+        while (num != 0) {
+            sum += num % 10;
+            num /= 10;
+        }
+        return sum;
+    }
+}
