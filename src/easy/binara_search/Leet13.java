2aaff8faa1c004358bbd25645f7db1c4f7f9c8f9
Ruslan Kurmanov <ruslan.kurmanov@idemia.com>
2020-02-11 15:52:05 +0100

    [11.02.2020] added last task

---
+++ src/easy/binara_search/Leet13.java
@@ -0,0 +1,11 @@
+package easy.binara_search;
+
+public class Leet13 {
+    public static void main(String[] args) {
+
+    }
+
+    public int romanToInt(String s) {
+
+    }
+}
--- src/easy/binara_search/Leet459.java
+++ src/easy/binara_search/Leet459.java
@@ -9,51 +9,78 @@ public class Leet459 {
         System.out.println(repeatedSubstringPattern("abaababaab"));
     }
 
+
+    //Solved with help
     public static boolean repeatedSubstringPattern(String s) {
-        StringBuilder stringBuilder = new StringBuilder();
-        for (int i = 0; i < s.length(); i++) {
-            if (!ifContains(s.charAt(i), stringBuilder.toString())) {
-                stringBuilder.append(s.charAt(i));
-            }
-        }
+        int n = s.length();
+        int half = n / 2;
 
-        if (s.length() % stringBuilder.length() != 0)
-            return false;
+        for (int i = 0; i < half; i++) {
 
-        int subStrLen = stringBuilder.length();
-        int count = 0;
-        StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < s.length(); i++) {
-            sb.append(s.charAt(i));
-            count++;
-            if (count == subStrLen) {
-                if (!checkSubString(stringBuilder.toString(), sb.toString()))
-                    return false;
-                count = 0;
-                sb.setLength(0);
+            if (n % (i + 1) != 0) continue;
+
+            int l = 0;
+            int r = i + 1;
+
+            while (s.charAt(l) == s.charAt(r)) {
+                if (r == s.length() - 1) return true;
+                r++;
+                l = (l + 1) % (i + 1);
             }
 
-
         }
 
-        return true;
-    }
-
-    static boolean ifContains(char c, String s) {
-        for (int i = 0; i < s.length(); i++) {
-            if (s.charAt(i) == c)
-                return true;
-        }
         return false;
-    }
 
-    static boolean checkSubString(String str1, String str2) {
-
-        System.out.println(str1 + " : " + str2);
-        for (int i = 0; i < str1.length(); i++) {
-            if (str1.charAt(i) != str2.charAt(i))
-                return false;
-        }
-        return true;
     }
 }
+
+//
+//    public static boolean repeatedSubstringPattern(String s) {
+//        StringBuilder stringBuilder = new StringBuilder();
+//        for (int i = 0; i < s.length(); i++) {
+//            if (!ifContains(s.charAt(i), stringBuilder.toString())) {
+//                stringBuilder.append(s.charAt(i));
+//            }
+//        }
+//
+//        if (s.length() % stringBuilder.length() != 0)
+//            return false;
+//
+//        int subStrLen = stringBuilder.length();
+//        int count = 0;
+//        StringBuilder sb = new StringBuilder();
+//        for (int i = 0; i < s.length(); i++) {
+//            sb.append(s.charAt(i));
+//            count++;
+//            if (count == subStrLen) {
+//                if (!checkSubString(stringBuilder.toString(), sb.toString()))
+//                    return false;
+//                count = 0;
+//                sb.setLength(0);
+//            }
+//
+//
+//        }
+//
+//        return true;
+//    }
+//
+//    static boolean ifContains(char c, String s) {
+//        for (int i = 0; i < s.length(); i++) {
+//            if (s.charAt(i) == c)
+//                return true;
+//        }
+//        return false;
+//    }
+//
+//    static boolean checkSubString(String str1, String str2) {
+//
+//        System.out.println(str1 + " : " + str2);
+//        for (int i = 0; i < str1.length(); i++) {
+//            if (str1.charAt(i) != str2.charAt(i))
+//                return false;
+//        }
+//        return true;
+//    }
+//}
