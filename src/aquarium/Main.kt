10b0008676fd5e7aaa895f196e2357b9a6738da1
Ruslan Kurmanov <ruslan.kurmanov@idemia.com>
2020-03-04 19:20:55 +0100

    Added last task which i solved

---
+++ src/aquarium/Main.kt
@@ -0,0 +1,15 @@
+package aquarium
+
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+
+
+fun main() {
+    (1..10000).forEach {
+        GlobalScope.launch {
+            val threadName = Thread.currentThread().name
+            println("$it printed on thread ${threadName}")
+        }
+    }
+    Thread.sleep(1000)
+}
